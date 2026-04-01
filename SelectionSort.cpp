247b64e3039b8434902c0f1dbd6ffca777a0f4e0
sudanpandey2024 <sudanpandey2024@gmail.com>
2026-04-01 09:02:41 +0545

    Selection Sort

---
+++ SelectionSort.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, minIndex, temp;
+
+    for(i = 0; i < n - 1; i++)
+    {
+        minIndex = i;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        if(minIndex != i)
+        {
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {5, 2, 9, 1, 6};
+    int n = 5;
+
+    printf("Original array:\n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
